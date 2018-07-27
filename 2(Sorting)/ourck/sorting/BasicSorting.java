9e532c6ac2eb22b3141c311aa86a7de4aabff541
OURCK <gxbhck39@live.com>
2018-07-27 20:38:12 +0800

    Add merge sorting

--- 2(Sorting)/ourck/sorting/BasicSorting.java
+++ 2(Sorting)/ourck/sorting/BasicSorting.java
@@ -5,21 +5,21 @@ public abstract class BasicSorting {
 	
 	abstract public void sort(Comparable[] a); // ByRef
 	
-	public boolean less(Comparable v, Comparable w) {
+	public static boolean less(Comparable v, Comparable w) {
 		return v.compareTo(w) < 0;
 	}
 	
-	public void exch(Comparable[] a, int i, int j) {
+	public static void exch(Comparable[] a, int i, int j) {
 		Comparable t = a[i]; a[i] = a[j]; a[j] = t;
 	}
 	
-	public void show(Comparable[] a) {
+	public static void show(Comparable[] a) {
 		for(int i = 0; i < a.length; i++)
 			System.out.print(a[i] + " ");
 		System.out.println();
 	}
 	
-	public boolean isSorted(Comparable[] a) {
+	public static boolean isSorted(Comparable[] a) {
 		for(int i = 0; i < a.length; i++)
 			if(less(a[i], a[i-1])) return false;
 		return true;
